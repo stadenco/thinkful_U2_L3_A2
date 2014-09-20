9f6d60b2ea8634e3cbc20dcd8181e00f1e75c1f9
Joel <stadenco@earthlink.net>
2014-09-19 22:51:52 -0400

    Initial Commit

---
+++ U2L2A2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  U2L2A2
+//
+//  Created by joelschonbach on 9/19/14.
+//  Copyright (c) 2014 joelschonbach. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
